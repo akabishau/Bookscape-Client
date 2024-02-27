98cc6c6096d36ca918e42bbee27390727cf31b2a
akabishau <aleksey.kabishau@icloud.com>
2024-02-27 09:26:40 -0500

    configure default layout and add placeholders to footer

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -1,3 +1,4 @@
+import styled from 'styled-components';
 import Header from './Header';
 import Footer from './Footer';
 
@@ -7,10 +8,18 @@ interface Props {
 
 export default function DefaultLayout({ children }: Props) {
   return (
-    <div>
+    <Wrapper>
       <Header />
-      {children}
+      <main>{children}</main>
       <Footer />
-    </div>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-rows: auto 1fr auto; /* header, main, footer */
+  align-items: start;
+  width: 100%;
+  height: 100vh;
+`;
--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,7 +1,24 @@
-export default function Footer() {
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
-    <footer>
-      <h1>Footer</h1>
-    </footer>
+    <StyledFooter>
+      <p>&copy; {currentYear} Bookscape</p>
+      <Link to="#">{'Feedback'}</Link>
+      <Link to="#">{'Privacy Policy'}</Link>
+    </StyledFooter>
   );
 }
+
+const StyledFooter = styled.footer`
+  display: grid;
+  grid-template-columns: 1fr auto auto;
+  justify-items: start;
+  align-items: center;
+  gap: 1rem;
+  height: 86px;
+`;
+
+export default Footer;
